c6a4209e0eb31103674e80e001c597935093bfee
ValeriaCarballo42 <valeriacarballob@gmail.com>
2026-05-03 22:38:36 +0000

    toDoList terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,10 +1,87 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-//create your first component
 const Home = () => {
+
+	const [list, setList] = useState([])
+	const [input, setInput] = useState("")
+
+	async function getTask() {
+		let result = await fetch("https://playground.4geeks.com/todo/users/Valeria123")
+		let data = await result.json()
+
+		setList(data.todos)
+		console.log(list)
+	}
+
+	async function addTask() {
+		let result = await fetch("https://playground.4geeks.com/todo/todos/Valeria123", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				"label": input,
+				"is_done": false,
+			})
+		})
+
+		await getTask()
+
+	}
+
+	async function deleteTask(id) {
+		let result = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "DELETE",
+		})
+
+		await getTask()
+	}
+
+	async function clearAll() {
+
+		list.forEach((tarea) => {
+			deleteTask(tarea.id)
+		})
+
+	}
+
+	useEffect(() => {
+		getTask()
+	}, [])
+
+
 	return (
-		<div className="text-center">
-            
+		<div className="container text-center mt-5 w-50">
+			<h1>Lista de tareas</h1>
+
+			<div>
+				<input
+					className=""
+					type="text"
+					placeholder="Añadir tarea..."
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							addTask()
+							setInput("")
+						}
+					}}
+				/>
+			</div>
+
+			<div className="w-50 mx-auto">
+				{list.map((tarea, index) => (
+					<div key={index} className="d-flex justify-content-between align-items-center border-bottom py-2 fs-5">
+						{tarea.label}
+
+						<button onClick={() => deleteTask(tarea.id)}>X</button>
+					</div>
+				))}
+			</div>
+			<div className="mt-3">
+				<button onClick={() => clearAll()}>Limpiar todo</button>
+			</div>
+
 		</div>
 	);
 };
