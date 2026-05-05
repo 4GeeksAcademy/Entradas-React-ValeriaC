0ad467f9fd80b6c9568f009f551f555dbf63f29a
ValeriaCarballo42 <valeriacarballob@gmail.com>
2026-05-05 21:05:30 +0000

    lista de tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,16 +6,19 @@ const Home = () => {
 	const [list, setList] = useState([])
 	const [input, setInput] = useState("")
 
+	const [editInput, setEditInput] = useState("")
+	const [editId, setEditId] = useState("")
+
+
 	async function getTask() {
-		let result = await fetch("https://playground.4geeks.com/todo/users/Valeria123")
+		let result = await fetch("https://playground.4geeks.com/todo/users/valeria77")
 		let data = await result.json()
 
 		setList(data.todos)
-		console.log(list)
 	}
 
 	async function addTask() {
-		let result = await fetch("https://playground.4geeks.com/todo/todos/Valeria123", {
+		let result = await fetch("https://playground.4geeks.com/todo/todos/valeria77", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
@@ -24,7 +27,7 @@ const Home = () => {
 			})
 		})
 
-		await getTask()
+		getTask()
 
 	}
 
@@ -33,7 +36,7 @@ const Home = () => {
 			method: "DELETE",
 		})
 
-		await getTask()
+		getTask()
 	}
 
 	async function clearAll() {
@@ -44,18 +47,32 @@ const Home = () => {
 
 	}
 
+	async function editTask(id) {
+		let result = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				"label": editInput,
+				"is_done": false,
+			})
+		})
+
+		getTask()
+	
+	}
+
 	useEffect(() => {
 		getTask()
 	}, [])
 
 
 	return (
-		<div className="container text-center mt-5 w-50">
-			<h1>Lista de tareas</h1>
+		<div className="container">
+			<h1 className="text-center my-3">Lista de tareas</h1>
 
 			<div>
 				<input
-					className=""
+					className="form-control form-control-lg"
 					type="text"
 					placeholder="Añadir tarea..."
 					value={input}
@@ -69,14 +86,38 @@ const Home = () => {
 				/>
 			</div>
 
-			<div className="w-50 mx-auto">
+			<div>
 				{list.map((tarea, index) => (
-					<div key={index} className="d-flex justify-content-between align-items-center border-bottom py-2 fs-5">
-						{tarea.label}
+					<div key={index} className="d-flex align-items-center border-bottom py-2">
+						<span className="me-auto fs-5">{tarea.label}</span>
 
-						<button onClick={() => deleteTask(tarea.id)}>X</button>
+						<button onClick={() => deleteTask(tarea.id)}>x</button>
+
+						<button data-bs-toggle="modal" data-bs-target="#editModal" 
+						onClick={() => {
+								setEditId(tarea.id)
+								setEditInput("")
+							}}
+							>Editar</button>
+						
+
 					</div>
 				))}
+
+				<div className="modal fade" id="editModal" tabIndex="-1">
+					<div className="modal-dialog">
+						<div className="modal-content p-3">
+							<input
+								type="text"
+								className="form-control mb-3"
+								placeholder="Editar tarea..."
+								value={editInput}
+								onChange={(e) => setEditInput(e.target.value)}
+							/>
+							<button className="btn btn-primary" onClick={() => editTask(editId)}>Guardar</button>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className="mt-3">
 				<button onClick={() => clearAll()}>Limpiar todo</button>
@@ -85,5 +126,6 @@ const Home = () => {
 		</div>
 	);
 };
+
 
 export default Home;
